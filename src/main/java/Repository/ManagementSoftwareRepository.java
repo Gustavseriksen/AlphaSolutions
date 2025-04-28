cbaa88523e1c17650d339a245e1729d5334b017b
BertramHansen <beha0007@stud.kea.dk>
2025-04-28 12:14:37 +0200

    initial commit with some repository features

--- src/main/java/Repository/ManagementSoftwareRepository.java
+++ src/main/java/Repository/ManagementSoftwareRepository.java
@@ -1,4 +1,58 @@
 package Repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ManagementSoftwareRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public ManagementSoftwareRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    //TODO
+    public void addProjectManager() {
+
+    }
+
+    //TODO
+    public void addEmployee() {
+
+    }
+
+    //TODO
+    public void addProject(){
+
+    }
+
+    //TODO
+    public void deleteProject(){
+
+    }
+
+    //TODO
+    public void editProject(){
+
+    }
+
+
+    //Subproject:
+
+    //TODO
+    public void addSubproject(){
+
+    }
+
+    //TODO
+    public void deleteSubproject(){
+
+    }
+
+    //TODO
+    public void editSubproject(){
+
+    }
+
 }
