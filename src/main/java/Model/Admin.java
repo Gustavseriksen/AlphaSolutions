17089e915bbea6ed336e895352102db0ba6c1502
gustav-hr <gure0001@stud.kea.dk>
2025-04-28 10:49:28 +0200

    admin, project & employee

--- src/main/java/Model/Admin.java
+++ src/main/java/Model/Admin.java
@@ -1,4 +1,16 @@
 package Model;
 
 public class Admin {
+
+    private int admin_id;
+    private String name;
+    private String password;
+
+    public Admin(int admin_id, String name, String password) {
+        this.admin_id = admin_id;
+        this.name = name;
+        this.password = password;
+    }
+
+
 }
