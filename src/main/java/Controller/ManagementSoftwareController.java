4b87806bcf1983cb2add458b0bceff47a7a0b026
gustav-hr <gure0001@stud.kea.dk>
2025-04-30 09:34:44 +0200

    service, controller & README

--- src/main/java/Controller/ManagementSoftwareController.java
+++ src/main/java/Controller/ManagementSoftwareController.java
@@ -1,15 +1,99 @@
 package Controller;
 
+import Service.ManagementSoftwareService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ManagementSoftwareController {
+    private final ManagementSoftwareService managementSoftwareService;
 
-    //Front Page View
+    public ManagementSoftwareController(ManagementSoftwareService managementSoftwareService) {
+        this.managementSoftwareService = managementSoftwareService;
+    }
+
+    // Front Page View
     @GetMapping("")
-
     public String viewFrontPage() {
+        return "index";
+    }
+
+    // Adding a pm & emp ---------------------------------------------------------------------
+    @PostMapping("/add-project-manager")
+    public String addProjectManager() {
+        managementSoftwareService.addProjectManager();
+        return "redirect:/"; // redirect to frontpage for now
+    }
+
+    @PostMapping("/add-employee")
+    public String addEmployee() {
+        managementSoftwareService.addEmployee();
+        return "redirect:/";
+    }
+
+    // Project -----------------------------------------------------------------------------
+    @PostMapping("/add-project")
+    public String addProject() {
+        managementSoftwareService.addProject();
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete-project")
+    public String deleteProject() {
+        managementSoftwareService.deleteProject();
+        return "redirect:/";
+    }
+
+    @PostMapping("/edit-project")
+    public String editProject() {
+        managementSoftwareService.editProject();
+        return "redirect:/";
+    }
+
+    // Subproject -------------------------------------------------------------------------
+    @PostMapping("/add-subproject")
+    public String addSubproject() {
+        managementSoftwareService.addSubproject();
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete-subproject")
+    public String deleteSubproject() {
+        managementSoftwareService.deleteSubproject();
+        return "redirect:/";
+    }
+
+    @PostMapping("/edit-subproject")
+    public String editSubproject() {
+        managementSoftwareService.editSubproject();
+        return "redirect:/";
+    } // -------------------------------------------------------------------------
+
+    // Task
+    @PostMapping("/add-task")
+    public String addTask() {
+        managementSoftwareService.addTask();
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete-task")
+    public String deleteTask() {
+        managementSoftwareService.deleteTask();
+        return "redirect:/";
+    }
+
+    @PostMapping("/edit-task")
+    public String editTask() {
+        managementSoftwareService.editTask();
+        return "redirect:/";
+    }
+
+    // Check if done
+    @GetMapping("/check-if-done")
+    public String checkIfDone(Model model) {
+        managementSoftwareService.checkIfDone();
         return "index";
     }
 }
--- src/main/java/Service/ManagementSoftwareService.java
+++ src/main/java/Service/ManagementSoftwareService.java
@@ -1,4 +1,66 @@
 package Service;
 
+import Repository.ManagementSoftwareRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ManagementSoftwareService {
+    private final ManagementSoftwareRepository managementSoftwareRepository;
+
+    public ManagementSoftwareService(ManagementSoftwareRepository managementSoftwareRepository) {
+        this.managementSoftwareRepository = managementSoftwareRepository;
+    }
+
+    // Worker
+    public void addProjectManager() {
+        managementSoftwareRepository.addProjectManager();
+    }
+
+    public void addEmployee() {
+        managementSoftwareRepository.addEmployee();
+    }
+
+    // Project
+    public void addProject() {
+        managementSoftwareRepository.addProject();
+    }
+
+    public void deleteProject() {
+        managementSoftwareRepository.deleteProject();
+    }
+
+    public void editProject() {
+        managementSoftwareRepository.editProject();
+    }
+
+    // Subproject
+    public void addSubproject() {
+        managementSoftwareRepository.addSubproject();
+    }
+
+    public void deleteSubproject() {
+        managementSoftwareRepository.deleteSubproject();
+    }
+
+    public void editSubproject() {
+        managementSoftwareRepository.editSubproject();
+    }
+
+    // Task
+    public void addTask() {
+        managementSoftwareRepository.addTask();
+    }
+
+    public void deleteTask() {
+        managementSoftwareRepository.deleteTask();
+    }
+
+    public void editTask() {
+        managementSoftwareRepository.editTask();
+    }
+
+    // Check
+    public void checkIfDone() {
+        managementSoftwareRepository.checkIfDone();
+    }
 }
